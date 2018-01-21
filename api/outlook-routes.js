df7c437ed36f5b2ee565ccfdf9c1f2d32c08ed36
Carlos Grijalva <grijalva.carlos@gmail.com>
2018-01-21 14:18:50 -0500

    Set up simple view page with Bootstrap

--- api/outlook-routes.js
+++ api/outlook-routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Coming from the outlook routes!');
+  res.render('home');
 });
 
 module.exports = router;
