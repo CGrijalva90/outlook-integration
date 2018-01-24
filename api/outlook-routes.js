082780421d2f0f3544e15e8e2339a3cc08218cc5
Carlos Grijalva <grijalva.carlos@gmail.com>
2018-01-24 13:54:10 -0500

    Update to send json of events to user

--- api/outlook-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authHelper = require('../authHelper');
-
-router.get('/', (req, res) => {
-  res.render('home', { link: authHelper.getAuthUrl() });
-});
-
-module.exports = router;
--- authHelper.js
+++ authHelper.js
@@ -21,10 +21,8 @@ const redirectUri = 'http://localhost:8000/authorize';
 const scopes = [
   'openid',
   'offline_access',
-  'User.Read',
   'Mail.Read',
   'Calendars.Read',
-  'Contacts.Read'
 ];
 
 function getAuthUrl() {
